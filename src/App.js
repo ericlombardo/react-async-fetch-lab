5a0abf02f1e8d0fa57a605c40cb81d69ae9a5920
Eric <eric.m.lombardo@gmail.com>
2021-08-05 16:51:26 -0400

    commit tree a273f556439cf613b2cbc39d17b72b4038b3b47f

--- src/App.js
+++ src/App.js
@@ -1,1 +1,16 @@
-// create your App component here
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  
+  componentDidMount() { // once component mounts invoke this lifecycle method
+    fetch('http://api.open-notify.org/astros.json') // fetch data
+    .then(resp => resp.json())
+    .then(people => console.log(people))  // log it once returned
+  }
+
+  render() { // render this when component is called
+    return (
+      <div></div>
+    )
+  }
+}
